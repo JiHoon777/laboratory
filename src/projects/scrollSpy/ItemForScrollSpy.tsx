0d12b134dc435cdbfac3e44deb250351feeff081
chase.song <chase.song@odcode.com>
2022-01-09 19:45:44 +0900

    scroll spy 구현

--- src/projects/scrollSpy/ItemForScrollSpy.tsx
+++ src/projects/scrollSpy/ItemForScrollSpy.tsx
@@ -3,18 +3,60 @@ import classNames from "classnames";
 
 interface IItemForScrollSpyProps {
   index: number;
+  setCurrentIndex: (currentIndex: number) => void;
+  addToSpyItems: (
+    index: number,
+    ele: React.MutableRefObject<HTMLDivElement>
+  ) => void;
 }
 
 export const ItemForScrollSpy: React.FC<IItemForScrollSpyProps> = (props) => {
-  const { index } = props;
+  const { index, setCurrentIndex, addToSpyItems } = props;
+  const itemRef = React.useRef<HTMLDivElement | null>(null);
+  const SpyTrigger = React.useRef<HTMLDivElement | null>(null);
+
+  const SpyObserver = new IntersectionObserver(
+    async ([{ isIntersecting }]) => {
+      if (isIntersecting) {
+        setCurrentIndex(index);
+        console.log("hi " + index);
+      }
+    },
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    }
+  );
+
+  React.useEffect(() => {
+    if (SpyTrigger.current) {
+      SpyObserver.observe(SpyTrigger.current);
+
+      return () => {
+        SpyTrigger.current && SpyObserver.unobserve(SpyTrigger.current);
+      };
+    }
+  }, [SpyTrigger.current]);
+
+  React.useEffect(() => {
+    if (!itemRef.current) {
+      return;
+    }
+
+    addToSpyItems(index, itemRef as React.MutableRefObject<HTMLDivElement>);
+  }, [itemRef.current]);
+
   return (
     <div
+      ref={itemRef}
       className={classNames(
-        "w-screen h-screen flex justify-center items-center"
+        "w-screen h-screen flex justify-center items-center relative"
       )}
       style={{ backgroundColor: "pink", borderBottom: "1px solid black" }}
     >
       <div>{index}</div>
+      <div ref={SpyTrigger} className={"absolute bottom-0 left-0"} />
     </div>
   );
 };
--- src/projects/scrollSpy/ScrollSpy.tsx
+++ src/projects/scrollSpy/ScrollSpy.tsx
@@ -1,38 +1,79 @@
 import React from "react";
 import { ItemForScrollSpy } from "./ItemForScrollSpy";
 import { useDebounce } from "../../hooks/useDebounce";
+import classNames from "classnames";
 
 interface IScrollSpyProps {}
 
 export const ScrollSpy: React.FC<IScrollSpyProps> = (props) => {
+  // scroll container ref
   const containerRef = React.useRef<HTMLDivElement | null>(null);
-  const { handleDebounce } = useDebounce();
+
+  // 영역별 ref 를 저장하기 위한 ref
+  const spyItems = React.useRef<{
+    [key: number]: React.MutableRefObject<HTMLDivElement>;
+  }>({});
+
+  // 현재 보고 있는 영역의 index
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+
+  const scrollToIndex = (index: number) => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    const top = spyItems.current[index]?.current.offsetTop ?? 0;
+    containerRef.current.scrollTo({ behavior: "smooth", top });
+  };
+
+  const addToSpyItems = (
+    index: number,
+    ele: React.MutableRefObject<HTMLDivElement>
+  ) => {
+    spyItems.current[index] = ele;
+  };
+
   const list = Array(10).fill("");
   return (
-    <div
-      className={" overflow-y-auto"}
-      ref={containerRef}
-      onScroll={(e) => {
-        if (containerRef.current) {
-          handleDebounce(
-            () => console.log(containerRef.current?.scrollTop),
-            300
-          );
-        }
-      }}
-    >
-      <div className={"fixed flex w-full"}>
+    <>
+      <div className={"flex w-full"}>
         {list.map((_, index) => {
           return (
-            <button key={index} className={"flex-1 bg-gray-100"}>
+            <button
+              key={index}
+              onClick={() => scrollToIndex(index)}
+              className={classNames("flex-1 bg-gray-100", {
+                "bg-gray-500": currentIndex === index,
+              })}
+            >
               {index}
             </button>
           );
         })}
       </div>
-      {list.map((_, index) => {
-        return <ItemForScrollSpy key={index} index={index} />;
-      })}
-    </div>
+      <div
+        className={"overflow-y-auto"}
+        ref={containerRef}
+        onScroll={(e) => {
+          // if (containerRef.current) {
+          //   handleDebounce(
+          //     () => console.log(containerRef.current?.scrollTop),
+          //     300
+          //   );
+          // }
+        }}
+      >
+        {list.map((_, index) => {
+          return (
+            <ItemForScrollSpy
+              key={index}
+              index={index}
+              setCurrentIndex={setCurrentIndex}
+              addToSpyItems={addToSpyItems}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
